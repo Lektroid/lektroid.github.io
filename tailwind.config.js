25db7954f3f2cb301923a27b07439f8a9f01964d
LektroiD <lektroid@gmail.com>
2024-12-05 00:05:30 +0000

    redone colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,31 @@
 // tailwind.config.js
 const colors = require('tailwindcss/colors')
 
+// module.exports = {
+//  theme: {
+//    colors: {
+//      transparent: 'transparent',
+//      current: 'currentColor',
+//      black: colors.black,
+//     white: colors.white,
+//     gray: colors.trueGray,
+//      indigo: colors.indigo,
+//      red: colors.rose,
+//      yellow: colors.amber,
+//      orange: colors.orange,
+//    }
+//  }
+//}
+
 module.exports = {
   theme: {
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      black: colors.black,
-      white: colors.white,
-      gray: colors.trueGray,
-      indigo: colors.indigo,
-      red: colors.rose,
-      yellow: colors.amber,
-      orange: colors.orange,
+    extend: {
+      colors: {
+        orange: '#FFA500',  // Customize the value as you want
+        // You can add more colors here
+      }
     }
-  }
+  },
+  variants: {},
+  plugins: [],
 }
